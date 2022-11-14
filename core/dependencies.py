58b85fbf8848e284978579a385798eda7e581c51
decapitator <root@localhost.local>
2022-11-14 13:49:04 +0400

    Complete

--- core/dependencies.py
+++ core/dependencies.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer
@@ -13,23 +15,24 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     insert_result = str()
     decode_token_result = await decode_token(token)
-    json_result = jsonable_encoder(decode_token_result)
-    user_in_db_result = await fetch_user_by_ext_id(ext_id=decode_token_result.sub)
+    # ' '.join(words)
+    user_in_db_result = await fetch_user_by_ext_id(ext_id=decode_token_result.get("sub"))
     if user_in_db_result is None:
         user_insert_data = UserInsert(
-            ext_id=decode_token_result.sub,
-            user_name=decode_token_result.preferred_username,
-            display_name=decode_token_result.preferred_username,
+            ext_id=decode_token_result.get("sub"),
+            user_name=decode_token_result.get("preferred_username"),
+            display_name=decode_token_result.get("preferred_username"),
             is_active=True,
         )
         insert_result: str = await insert_user(data=user_insert_data)
+    print(user_in_db_result)
     user: User = User(
-        id=user_in_db_result is None if insert_result else user_in_db_result.id,
-        ext_id=decode_token_result.sub,
-        display_name=decode_token_result.preferred_username,
-        roles=decode_token_result.realm_access.roles,
+        id=user_in_db_result is None if insert_result else str(user_in_db_result),
+        ext_id=decode_token_result.get("sub"),
+        display_name=decode_token_result.get("preferred_username"),
+        roles=decode_token_result.get("realm_access").get("roles"),
         is_active=False,
-        user_name=decode_token_result.preferred_username)
+        user_name=decode_token_result.get("preferred_username"))
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import typing
 
 import jwt
@@ -11,7 +12,7 @@ async def decode_token(encoded: str) -> typing.Any:
         'verify_exp': False,  # Skipping expiration date check
         'verify_aud': False
     }
-    public_key_cert = b"-----BEGIN CERTIFICATE-----\n3ug==\n-----END CERTIFICATE-----"
+    public_key_cert = "-----BEGIN CERTIFICATE-----\nMIIClzCCAX8CBgGEcGBSXzANBgkqhkiG9w0BAQsFADAPMQ0wCwYDVQQDDARmYXB5MB4XDTIyMTExMzA5NDI1MVoXDTMyMTExMzA5NDQzMVowDzENMAsGA1UEAwwEZmFweTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAIMcKfI1wwT/7z+ulGSTu27TSzQiuP86YzoGRXZAyR/+8/8rdYKX7zYNqwi75tRWHtU32rKzs1FC2k4aBYlrDxRsfjKkAz0b6nmHMPtphOQEQZz/yL1nupHxBWs1sOl8pe9MX0aCbDZr4HV9sV9f7uRkEWC4C18wy6LLYFbJctYk0+U03AXZqvkKmIfUyZFYjw/9t767+PFbpw3AKwxvmAse2E1UFb/QPsAzcTsfBp0Z6nTJHGbTQX1TqDvkZ8oLPYocjHw09qE8iZMf0UKYj5RPReulLmgJUc+lQzhKHlujekg1V7FTpZt6iR/qXBH0mK4Y26lAqUvC47KTgOFwM7sCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAUDZ1YoF+mMpzg3h6u6VC4d8iEW/4SkKlgU5y51bjrLu5WlmTZxFI5lfOTDnnHWYLacrXRYyIVZarBd87lH0EzSN1Cs416xMaQCZVHhPzqj0Uzdb6ojOFPXcLqDLR48VeRQVgBuLHx/l44mP95QbwKjGuL6pHFR1/ivuAK5hi3A8drx8PLLxPZNzg/L1bXtqTfisvKOM8zsPCSiZZMGjZ97feZ6yIPsWEBE8q/f8vycYYG7jcIlXxd3gF3Vsovpn8cVA71c9eimd6x2c2oF5TybRkMSvPNBHCnY2txCmXR043zA2Esj/Gqj7rqeOPyxTC0KZuvvN5F7ciOXB0IN73ug==\n-----END CERTIFICATE-----".encode('utf-8')# os.getenv("KEYCLOAK_CERT").encode()
     cert_obj = load_pem_x509_certificate(public_key_cert)
     public_key = cert_obj.public_key()
     result = jwt.decode(encoded, public_key, algorithms=["RS256"], options=options)
